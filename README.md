7e608bcbb2bfbb54db6b7421f6a19c3a538334cb
unknown <ivy.dokkaebi@gmail.com>
2019-08-26 16:03:18 -0500

    Se modifico a rutas estándares

---
+++ README.md
@@ -0,0 +1,4 @@
+# AutomaticStartTasks
+My customize simple start batch script
+
+Crate a shortcut and set the .ico file :D
---
+++ script.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+TITLE Automatizador
+
+start C:\xampp\xampp-control.exe
+start chrome
+start code
+start cmd /k "cd.. && cd .. && cd .."
+exit
